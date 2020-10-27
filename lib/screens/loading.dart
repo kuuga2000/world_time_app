2c40a9ae15d9310f326f5bf767d4263a5b412596
Kuuga <ryuki.servaiv@gmail.com>
2020-10-27 23:59:16 +0700

    WorldTime Custom Class

--- lib/screens/loading.dart
+++ lib/screens/loading.dart
@@ -1,6 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
-import 'dart:convert';
+import 'package:world_time_app/services/world_time.dart';
 
 class Loading extends StatefulWidget {
   @override
@@ -9,30 +9,31 @@ class Loading extends StatefulWidget {
 
 class _LoadingState extends State<Loading> {
 
-  void getTime() async {
-    Response response = await get('http://worldtimeapi.org/api/timezone/Asia/Tomsk');
-    Map data = jsonDecode(response.body);
-    String datetime = data['datetime'];
-    String offset = data['utc_offset'].substring(1,3);
+  String time = 'loading';
 
-    DateTime now = DateTime.parse(datetime);
-    now = now.add(Duration(hours: int.parse(offset)));
-
-    print(now);
-
+  void setupWorldTime() async {
+    WorldTime instance = WorldTime(location: 'Berlin', flag: 'germany.png', url: 'Europe/Berlin');
+    await instance.getTime();
+    print(instance.time);
+    setState(() {
+      time = instance.time;
+    });
   }
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
-    getTime();
+    setupWorldTime();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('Loading Screen')
+      body: Padding(
+        padding: EdgeInsets.all(50.0),
+        child: Text(time)
+      )
     );
   }
 }
---
+++ lib/services/world_time.dart
@@ -0,0 +1,27 @@
+import 'package:http/http.dart';
+import 'dart:convert';
+
+class WorldTime {
+  //location name for the UI
+  String location;
+  //time in the location
+  String time;
+  //url to an asset flag icon
+  String flag;
+  //location url for api endpoint
+  String url;
+
+  WorldTime({ this.location, this.flag, this.url });
+
+  Future<void> getTime() async {
+    Response response = await get('http://worldtimeapi.org/api/timezone/${url}');
+    Map data = jsonDecode(response.body);
+    String datetime = data['datetime'];
+    String offset = data['utc_offset'].substring(1,3);
+
+    DateTime now = DateTime.parse(datetime);
+    now = now.add(Duration(hours: int.parse(offset)));
+
+    time = now.toString();
+  }
+}
